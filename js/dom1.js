5b727e80bd61ab2ce05aa2fc2236c3a24108e8f7
k.maheshkunta@gmail.com <k.maheshkunta@gmail.com>
2023-06-05 10:57:40 +0530

    chnages done i=in dom getelemnetsbt classname

--- js/dom1.js
+++ js/dom1.js
@@ -12,9 +12,26 @@
 
 
 //GETELEMENTBYID
-var headerTitle=document.getElementById('header-title');
-var head=document.getElementById('main-header');
-var item=document.getElementById('item');
-item.innerHTML='<h2 style="color:green"><b>Add Items</b></h2>';
-console.log(headerTitle);
-head.style.borderBottom="solid 3px #000";
+//var headerTitle=document.getElementById('header-title');
+//var head=document.getElementById('main-header');
+//var item=document.getElementById('item');
+//item.innerHTML='<h2 style="color:green"><b>Add Items</b></h2>';
+//console.log(headerTitle);
+//head.style.borderBottom="solid 3px #000";
+
+//GETelement by classsname
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+items[1].textContent='hello';
+items[1].style.fontWeight='bold';
+//items[1].style.backgroundColor='yellow';
+//Make the 3 rd element in the list have green background color
+
+items[2].style.backgroundColor='green';
+
+//Make all the elements in the list have bold color font
+for(let i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
+
+
