901c68ca0d392f5a9b4007074a82154ba0a7f42b
k.maheshkunta@gmail.com <k.maheshkunta@gmail.com>
2023-06-05 15:51:28 +0530

    changed dom1 js by getelements by tag name

--- js/dom1.js
+++ js/dom1.js
@@ -20,7 +20,7 @@
 //head.style.borderBottom="solid 3px #000";
 
 //GETelement by classsname
-var items=document.getElementsByClassName('list-group-item');
+/*var items=document.getElementsByClassName('list-group-item');
 console.log(items);
 items[1].textContent='hello';
 items[1].style.fontWeight='bold';
@@ -32,6 +32,26 @@ items[2].style.backgroundColor='green';
 //Make all the elements in the list have bold color font
 for(let i=0;i<items.length;i++){
     items[i].style.fontWeight='bold';
+}*/
+
+// editing it with getelementsbyclassname and then by getelementbytagname
+//gateelenetsbyclassname doesnt have access to item 5 due to diiference in class name
+//now with getelementbytagnsme
+
+var li=document.getElementsByTagName('li');
+console.log(li);
+li[1].textContent='hello';
+li[1].style.fontWeight='bold';
+//items[1].style.backgroundColor='yellow';
+//Make the 3 rd element in the list have green background color
+
+li[2].style.backgroundColor='green';
+
+//Make all the elements in the list have bold color font
+for(let i=0;i<li.length;i++){
+    li[i].style.fontWeight='bold';
 }
+//done with changes using getselementsby tag name;
+
 
 
