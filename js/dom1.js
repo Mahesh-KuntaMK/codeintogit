bdc59ea6005aa21c560878f9b2d611ebca0fb247
k.maheshkunta@gmail.com <k.maheshkunta@gmail.com>
2023-06-07 11:36:39 +0530

    creating nodes and modifying Dom

--- js/dom1.js
+++ js/dom1.js
@@ -74,14 +74,130 @@ for(let i=0;i<li.length;i++){
 //1.Using QuerySelectorALL change the font color to green for 2nd item in the item list
 //2.Choose all the odd elements and make their background green using QuerySelectorALL
   
-var  items=document.querySelectorAll('li');
-console.log(items);
-items[1].style.color='green';
-var odd=document.querySelectorAll('li:nth-child(odd');
-for(var i=0;i<odd.length;i++){
+//var  items=document.querySelectorAll('li');
+//console.log(items);
+//items[1].style.color='green';
+//var odd=document.querySelectorAll('li:nth-child(odd');
+//for(var i=0;i<odd.length;i++){
 
-    odd[i].style.backgroundColor='green';
-}
+    //odd[i].style.backgroundColor='green';
+//}
 //done the changes successfully using quartSelectorAll
+
+
+//now lets practice some queary slector methods
+//1.parentElemnet
+var itemList=document.querySelector('#items');
+//console.log(itemList.parentElement.parentElement.parentElement);
+ 
+//Now 2.parentNode
+//console.log(itemList);
+//itemList.parentNode.parentNode.style.backgroundColor="green";4
+
+//console.log(itemList.parentElement.parentElement.parentElement);
+//itemList.parentElement.parentElement.style.backgroundColor='red';
+
+//CHILDNODES
+//console.log(itemList.childNodes);
+//console.log(itemList.children);
+//console.log(itemList.children[1]);
+//itemList.children[1].style.backgroundColor='green';
+
+
+//firstChild
+//console.log(itemList.firstChild);
+
+//firstElementChild
+//console.log(itemList.firstElementChild);
+//itemList.firstElementChild.style.backgroundColor="yellow";
+//itemList.firstElementChild.textContent="helloWorld"
+
+
+//lastchild
+//console.log(itemList.lastChild);
+//lastElementChild
+//console.log(itemList.lastElementChild);
+//itemList.lastElementChild.textContent="hello new world"
+
+
+//NEXTSIBLING
+//console.log(itemList.nextSibling);
+
+//NEXTELEMENTSIBLING
+//console.log(itemList.nextElementSibling);//here we dont have sibling
+
+//previousSibling
+//console.log(itemList.previousSibling);
+
+//previousElementSibling
+//console.log(itemList.previousElementSibling);
+//itemList.previousElementSibling.style.backgroundColor="red";
+
+
+//lets learn how to create in js using dom methods;
+
+//createElement and added "hello" before ItemlIster
+
+var newDiv=document.createElement("div");
+
+//add class
+newDiv.className="hello"
+
+//add id
+newDiv.id='hello1'
+
+//add attribute
+
+
+newDiv.setAttribute('title','Hello Div');
+
+//add text node
+
+var newDivText=document.createTextNode('Hello');
+
+
+//add text to div
+newDiv.appendChild(newDivText);
+
+
+
+var container=document.querySelector('header .container');
+
+var h1=document.querySelector('header h1');
+
+console.log(newDiv);
+
+
+
+container.insertBefore(newDiv,h1);
+
+//now lets add hello to item
+
+var newText=document.createElement("Div");
+
+
+newText.className="item1";
+
+newText.id='hello'
+
+var text=document.createTextNode("hello");
+
+newText.appendChild(text);
+
+
+var ul=document.querySelector('div .list-group');
+var li=ul.firstElementChild;
+
+   
+
+
+ul.insertBefore(newText,li);
+
+//successfully complted dom methods using methods of querySElector;
+
+
+
+
+
 
 
